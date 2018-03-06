9bff7e5bf4a005a1427bbec35de2c6434186c18f
ShiroAimai <33904974+ShiroAimai@users.noreply.github.com>
2018-03-06 19:00:20 +0100

    Add files via upload

---
+++ PHP/login.php
@@ -0,0 +1,20 @@
+<?php
+	session_start();
+	if($_POST['codice']=='admin' && $_POST['pass']=='admin')
+	{
+		$_SESSION['wrong_login']=0;
+		$_SESSION['logged']= 1;
+		header("Location:../HTML/Calendario.html");
+	}
+	else if($_POST['codice']=='utente' && $_POST['pass']=='utente')
+		{
+			$_SESSION['wrong_login']=0;
+			$_SESSION['logged']= 1;
+			header("Location:../HTML/Attivita.html");
+		}
+		else
+		{
+			$_SESSION['wrong_login']= 1; // flag degli errori: sbaglio le credenziali, ricarico la pagina adminLogin.php con il mess di errore.
+			header("Location:login2.php");
+		}
+?>
