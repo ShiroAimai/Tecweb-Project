56c07c11af5e3211fd491ae0334b264a6cbceb9c
Cesco94 <francesco.sacchetto.2@gmail.com>
2018-06-19 10:41:53 +0200

    Sistemata immaginesingola.php per dividere php da html

--- PHP/immaginesingola.php
+++ PHP/immaginesingola.php
@@ -1,18 +1,16 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"  xml:lang="it" lang="it">
-<head>
-	<title> Galleria Foto | Body Evolution</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<link href="../CSS/Desktop.css" rel="stylesheet" type="text/css" media="handheld, screen"/>
-	<link href="../CSS/Mobile.css" rel="stylesheet" type="text/css" media="handheld, screen and (max-width:480px), only screen and (max-device-width:480px)"/>
-	<link href="../CSS/Print.css" rel="stylesheet" type="text/css" media="print"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-</head>
-
-<body>
-	<div class="sfondonero">
 		<?php
+
+			$head = file_get_contents("../Templates/headerImmagineSingola.txt");
+			echo $head;
+
+			$body1 = "<body>";
+			$body2 = "</body>";
+			$div1 = "<div class=\"sfondonero\">";
+			$div2 = "</div>";
+
+			echo $body1;
+			echo $div1;
+
 			$aux=$_GET['album'];
 			$stampa="
 			<div class=\"backbutton\">
@@ -21,7 +19,9 @@
 			</a></div>";
 			$stampa.="<img class=\"imgsfondonero\" src=\"../galleria/".$_GET['nome']."\" alt=\"Immagine dell'album\"/>";
 			echo $stampa;
+
+			echo $div2;
+			echo $body2;
+			$closehtml = "</html>";
+			echo $closehtml;
 		?>
-	</div>
-</body>
-</html>
