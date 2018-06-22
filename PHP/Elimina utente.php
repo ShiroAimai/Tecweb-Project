9520247f4f1362072fb6410c867b98d668235c5e
snordio <stefanonordio96@gmail.com>
2018-06-22 15:16:24 +0200

    Aggiunto messaggio errore nel caso di admin che tenta di eliminare il suo account

--- PHP/Elimina utente.php
+++ PHP/Elimina utente.php
@@ -1,6 +1,10 @@
 <?php
 require_once('config.php');
 register('user');
-query("DELETE FROM utente WHERE CodiceUtente=$user");
-header("Location:Lista utenti.php");
+if($_SESSION['user_code'] == $user) {
+	header("Location:Errore eliminazione utente.php");
+} else {
+	query("DELETE FROM utente WHERE CodiceUtente=$user");
+	header("Location:Lista utenti.php");
+}
 ?>
---
+++ PHP/Errore eliminazione utente.php
@@ -0,0 +1,31 @@
+<?php
+	require_once('config.php');
+    $head = file_get_contents("../Templates/headerAddUser.txt");
+    $foot = file_get_contents("../Templates/footer.txt");
+	$deleteError = file_get_contents("../Templates/DeleteError.txt");
+	$logout = "<button id=\"logoutButton\" onclick=\"window.location.href='logout.php'\">Logout</button>";
+	$login = "<button onclick=\"window.location.href='../HTML/AreaPersonale.html'\">Area Personale</button>";
+	$adminPanel = "<button onclick=\"window.location.href='AdminPanel.php'\">Admin Panel</button>";
+	$userPanel = "<button onclick=\"window.location.href='UserPanel.php'\">User Panel</button>";
+	$closediv = "</div>";
+	$closebody = "</body>";
+	$closehtml = "</html>";
+	
+	echo $head;
+	if(isset($_SESSION['user_code']) && $_SESSION['user_type'] == 'admin') {
+		echo $logout;
+		echo $adminPanel;
+	} else if(isset($_SESSION['user_code']) && $_SESSION['user_type'] == 'user') {
+		echo $logout;
+		echo $userPanel;
+	} else {
+		echo $login;
+	}
+	
+	echo $closediv;
+	echo $closediv;
+	echo $deleteError;
+	echo $foot;
+	echo $closebody;
+	echo $closehtml;
+?>
