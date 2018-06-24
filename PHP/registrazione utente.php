e6e6d9687cac210302c0100a9d697e3f5af948ff
snordio <stefanonordio96@gmail.com>
2018-06-24 14:44:07 +0200

    Rimosso codice utente nelle form di inserimento admin/user, ora si auto-imposta con auto-increment nel db

--- PHP/registrazione utente.php
+++ PHP/registrazione utente.php
@@ -1,14 +1,13 @@
 <?php
 
 	require_once('config.php');
-	register('userCode');
 	register('userName');
 	register('userSurname');
 	register('pass');
 	register('userMail');
 	$user = "user";
-	$sql = "INSERT INTO utente (CodiceUtente, Nome, Cognome, Password, Email, Tipo)
-	VALUES ('$userCode','$userName','$userSurname','$pass','$userMail','$user')";
+	$sql = "INSERT INTO utente (Nome, Cognome, Password, Email, Tipo)
+	VALUES ('$userName','$userSurname','$pass','$userMail','$user')";
 	query($sql);
 
 	header("Location:utente aggiunto.php");
