4b1eff2a41f3c7ebc85c42cd86271585817c6091
ShiroAimai <shiroaimai@gmail.com>
2018-06-24 12:44:30 +0200

    fix bugs in form fattura

--- PHP/Aggiungi fattura.php
+++ PHP/Aggiungi fattura.php
@@ -16,15 +16,15 @@
 	VALUES ('$userCode','$today','$importo','$mesi','$punti')";
 	query($sql);
 	if(isset($corsiselect1))
-		query("INSERT INTO iscrizionecorso (CodiceUtente, CodiceCorso, NomeCorso) VALUES ('$usercode,'1','$corsiselect1')");
+		query("INSERT INTO iscrizionecorso (CodiceUtente, CodiceCorso, NomeCorso) VALUES ($userCode, 1, $corsiselect1)");
 	if(isset($corsiselect2))
-		query("INSERT INTO iscrizionecorso (CodiceUtente, CodiceCorso, NomeCorso) VALUES ('$usercode,'5','$corsiselect2')");
+		query("INSERT INTO iscrizionecorso (CodiceUtente, CodiceCorso, NomeCorso) VALUES ($userCode, 5, $corsiselect2)");
 	if(isset($corsiselect3))
-		query("INSERT INTO iscrizionecorso (CodiceUtente, CodiceCorso, NomeCorso) VALUES ('$usercode,'4','$corsiselect3')");
+		query("INSERT INTO iscrizionecorso (CodiceUtente, CodiceCorso, NomeCorso) VALUES ($userCode, 4, $corsiselect3)");
 	if(isset($corsiselect4))
-		query("INSERT INTO iscrizionecorso (CodiceUtente, CodiceCorso, NomeCorso) VALUES ('$usercode,'3','$corsiselect4')");
+		query("INSERT INTO iscrizionecorso (CodiceUtente, CodiceCorso, NomeCorso) VALUES ($userCode, 3, $corsiselect4)");
 	if(isset($corsiselect5))
-		query("INSERT INTO iscrizionecorso (CodiceUtente, CodiceCorso, NomeCorso) VALUES ('$usercode,'2','$corsiselect5')");
+		query("INSERT INTO iscrizionecorso (CodiceUtente, CodiceCorso, NomeCorso) VALUES ($userCode, 2, $corsiselect5)");
 	
 
 	header("Location:fattura aggiunta.php");
