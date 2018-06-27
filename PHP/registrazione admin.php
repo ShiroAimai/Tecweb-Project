4dad7fc98fb9a9be8aa4e6da2be3e9a33e977d09
ShiroAimai <shiroaimai@gmail.com>
2018-06-27 12:30:40 +0200

    added validation input form add user e admin

--- PHP/registrazione admin.php
+++ PHP/registrazione admin.php
@@ -6,7 +6,7 @@
 	register('userMail');
 	$admin = "admin";
 
-	if(isset($userName) && !empty($userName) && isset($userSurname) && !empty($userSurname) && isset($pass) && !empty($pass) && isset($userMail) && !empty($userMail)) {
+	if(isset($userName) && !empty($userName) && preg_match("/^[a-zA-Z ]*$/",$userName) && isset($userSurname) && !empty($userSurname) && preg_match("/^[a-zA-Z ]*$/",$userSurname) && isset($pass) && !empty($pass) && isset($userMail) && !empty($userMail) && filter_var($userMail, FILTER_VALIDATE_EMAIL)) {
 		$userName = test_input($userName);
         $userSurname = test_input($userSurname);
 		$pass = test_input($pass);
--- PHP/registrazione utente.php
+++ PHP/registrazione utente.php
@@ -6,7 +6,7 @@
 	register('userMail');
 	$user = "user";
 
-	if(isset($userName) && !empty($userName) && isset($userSurname) && !empty($userSurname) && isset($pass) && !empty($pass) && isset($userMail) && !empty($userMail)) {
+	if(isset($userName) && preg_match("/^[a-zA-Z ]*$/",$userName) && !empty($userName) && isset($userSurname) && !empty($userSurname) && preg_match("/^[a-zA-Z ]*$/",$userSurname) && isset($pass) && !empty($pass) && isset($userMail) && filter_var($userMail, FILTER_VALIDATE_EMAIL) && !empty($userMail)) {
 		$userName = test_input($userName);
         $userSurname = test_input($userSurname);
 		$pass = test_input($pass);
