ebb8845bc55689efd316328c6c3c4447b6e981ab
JimVentura <marcomasiero92@gmail.com>
2018-06-30 17:27:42 +0200

    fix stylesheet

--- PHP/printHeader.php
+++ PHP/printHeader.php
@@ -7,9 +7,9 @@
 			<title>".$title." | Body Evolution</title>
 			<meta charset=\"utf-8\">
 			<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>
-			<link href=\"../CSS/Desktop.css\" rel=\"stylesheet\"/>
-			<link href=\"../CSS/Tablet.css\" rel=\"stylesheet\" media=\"(min-width:768px) and (max-width:1024px)\"/>
-			<link href=\"../CSS/Mobile.css\" rel=\"stylesheet\" media=\"(max-width: 767px)\"/>
+			<link href=\"../CSS/Desktop.css\" rel=\"stylesheet\" media=\"screen\"/>
+			<link href=\"../CSS/Tablet.css\" rel=\"stylesheet\" media=\"screen and (min-width:768px) and (max-width:1024px)\"/>
+			<link href=\"../CSS/Mobile.css\" rel=\"stylesheet\" media=\"screen and (max-width: 767px)\"/>
 			<link href=\"../CSS/Print.css\" rel=\"stylesheet\" media=\"print\"/>
 			<link href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\" rel=\"stylesheet\"/>
 		</head>
