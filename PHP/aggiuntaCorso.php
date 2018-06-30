d97114a86ab625a6d85898a12fe6bae764348197
snordio <stefanonordio96@gmail.com>
2018-06-30 10:42:49 +0200

    Risolto bug iscrizione/disiscrizione corsi

--- PHP/aggiuntaCorso.php
+++ PHP/aggiuntaCorso.php
@@ -3,11 +3,17 @@
 	$userCode = $_SESSION['user_code'];
     $counter=1;
 
-	while(isset($_POST["select".$counter.""])) {
-		$codiceCorso = select("SELECT * FROM corso WHERE NomeCorso='".$_POST["select".$counter.""]."'");
-		query("INSERT INTO iscrizionecorso (CodiceUtente, CodiceCorso, NomeCorso) VALUES ('$userCode', '".$codiceCorso[0]['CodiceCorso']."', '".$_POST["select".$counter.""]."')");
-		$counter++;
-    }
+	if (isset($_POST['submit'])) {
+		while(isset($_POST["select".$counter.""])) {
+			$codiceCorso = select("SELECT * FROM corso WHERE NomeCorso='".$_POST["select".$counter.""]."'");
+			query("INSERT INTO iscrizionecorso (CodiceUtente, CodiceCorso, NomeCorso) VALUES ('$userCode', '".$codiceCorso[0]['CodiceCorso']."', '".$_POST["select".$counter.""]."')");
+			$counter++;
+		}
+	}
+	else {
+		header("Location: operazioneFallita.php");
+		die();
+	}
 	close_connection();
     header("Location: operazioneCorsoAvvenuta.php?attr=0");
 ?>
--- PHP/rimozioneCorso.php
+++ PHP/rimozioneCorso.php
@@ -3,10 +3,16 @@
 	$userCode = $_SESSION['user_code'];
     $counter=1;
 
-	while(isset($_POST["select".$counter.""])) {
-		query("DELETE FROM iscrizionecorso WHERE CodiceUtente='$userCode' AND NomeCorso='".$_POST["select".$counter.""]."'");
-		$counter++;
-    }
+	if (isset($_POST['submit'])) {
+		while(isset($_POST["select".$counter.""])) {
+			query("DELETE FROM iscrizionecorso WHERE CodiceUtente='$userCode' AND NomeCorso='".$_POST["select".$counter.""]."'");
+			$counter++;
+		}
+	}
+	else {
+		header("Location: operazioneFallita.php");
+		die();
+	}
 	close_connection();
     header("Location: operazioneCorsoAvvenuta.php?attr=1");
 ?>
